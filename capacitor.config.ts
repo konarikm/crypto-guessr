6865be25be9b8a975881daccdadfa4cec48126ac
Martin Konarik <m1_konarik@utb.cz>
2024-12-09 20:59:51 +0100

    add capacitor packages for android support, splashscreen support and share functionality

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cz.utb.fai.cryptoguessr',
   appName: 'CryptoGuessr',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1500,
+      // backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      // androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      // androidSpinnerStyle: "large",
+      // iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
